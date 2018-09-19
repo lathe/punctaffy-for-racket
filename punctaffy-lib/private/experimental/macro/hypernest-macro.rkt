4643200031613b1bdcd9913966cbda4c08b16709
Ross Angle <rokitna@hotmail.com>
2018-09-19 04:50:31 -0700

    Copy hypertee-macro.rkt and qq.rkt to make hypernest-macro.rkt and hypernest-qq.rkt. This will be where we develop an approach that actually generalizes to higher-quasiquotation-shaped operators by using hypernests instead of hypertees.

---
+++ punctaffy-lib/private/experimental/macro/hypernest-macro.rkt
@@ -0,0 +1,337 @@
+#lang parendown racket/base
+
+; hypertee-macro.rkt
+;
+; A framework for macros which take hypersnippet-shaped syntax.
+
+;   Copyright 2018 The Lathe Authors
+;
+;   Licensed under the Apache License, Version 2.0 (the "License");
+;   you may not use this file except in compliance with the License.
+;   You may obtain a copy of the License at
+;
+;       http://www.apache.org/licenses/LICENSE-2.0
+;
+;   Unless required by applicable law or agreed to in writing,
+;   software distributed under the License is distributed on an
+;   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+;   either express or implied. See the License for the specific
+;   language governing permissions and limitations under the License.
+
+
+(require #/only-in racket/contract/base -> any/c list/c)
+(require #/only-in racket/contract/region define/contract)
+(require #/only-in syntax/parse id syntax-parse)
+
+(require #/only-in lathe-comforts dissect expect fn mat w- w-loop)
+(require #/only-in lathe-comforts/list list-foldr list-map)
+(require #/only-in lathe-comforts/maybe
+  just maybe? maybe-bind maybe-map nothing)
+(require #/only-in lathe-comforts/struct struct-easy)
+(require #/only-in lathe-comforts/trivial trivial)
+(require #/only-in lathe-ordinals onum-omega)
+
+(require #/only-in punctaffy/hypersnippet/hypertee
+  degree-and-closing-brackets->hypertee hypertee?
+  hypertee-bind-one-degree)
+
+(provide
+  (struct-out ht-tag-1-s-expr-stx)
+  (struct-out ht-tag-1-other)
+  (struct-out ht-tag-2-list)
+  (struct-out ht-tag-2-list*)
+  (struct-out ht-tag-2-vector)
+  (struct-out ht-tag-2-prefab)
+  (struct-out ht-tag-2-other)
+  s-expr-stx->ht-expr
+  simple-ht-builder-syntax)
+
+
+; TODO: We're going to take this approach:
+;
+;
+; Conceptually, we'll treat the program's syntax as a degree-omega
+; hypertee built up from a sequential encoding. That is, the program's
+; syntax is an arrangement of "holes with holes with holes..." of
+; various degrees, and the particular holes we're using can always be
+; flattened into a sequence of degree-annotated parentheses.
+;
+; Since we're in Racket, our syntax transformers will take
+; s-expression-shaped syntax objects as input like usual. When we
+; write a syntax transformer where we care to process
+; higher-dimensional syntax, we will explicitly convert these syntax
+; objects to degree-omega hypertees using a special macroexpansion
+; procedure called `s-expr-stx->ht-expr`. We call the degree-omega
+; hypertees "ht-expressions" when we use them for syntax this way.
+;
+; This conversion is a kind of macroexpansion only because we don't
+; hardcode particular symbols for the higher-dimensional brackets that
+; this conversion process handles. Instead of hardcoding those
+; symbols, we allow users to define their own
+; syntax-object-to-ht-expression transformers. These are a lot like
+; reader extensions, since they create higher-dimensional syntax out
+; of a lower-dimensional encoding.
+;
+;
+; Of all the Racket syntax transformers that will invoke
+; `s-expr-stx->ht-expr`, the most familiar will be ones that imitate
+; `quasiquote` or `quasisyntax`. Unfortunately, these operations are
+; somewhat deceptive examples because they distract us with another
+; idiosyncratic behavior: They use the higher-dimensional structure of
+; their syntax to construct a *lower-dimensional* data structure, and
+; in the process they have to re-encode some of that
+; higher-dimensional structure in terms of bracket symbols. The
+; output's bracket symbols can't always correspond exactly to the ones
+; the user entered in the first place (that is, if we want the output
+; to be capable of containing any arrangement of symbols the user
+; needs), but in general it's very preferable for them to be as close
+; an approximation to the input's brackets as possible. That way the
+; user can maintain quoted code using the same syntax they use to
+; maintain other code, without constantly escaping every bracket they
+; write.
+;
+; This means for the sake of `quasiquote` and `quasisyntax` operations
+; in particular,  when we parse low-dimensional syntax into
+; high-dimensional syntax, we also need to *preserve* enough details
+; to reconstruct the original; we need some amount of round-tripping
+; support.
+;
+; So, should we just set aside `quasiquote` and `quasisyntax` until we
+; talk about other examples? Not necessarily, because even our
+; simplest examples will tend to encounter *some* of the same
+; idiosyncrasies: We eventually have to compile every operation to
+; Racket code, which is a low-dimensional data structure again. The
+; input format of many of our operations will consist of snippets of
+; Racket code, and if these inputs are used like patchwork to generate
+; some Racket code (as traditional Racket syntax transformers often
+; do), then those parts of the output should be a perfectly faithful
+; recreation of the input; any imperfection will be visible to Racket
+; syntax transformer calls occurring in that snippet.
+;
+; So now we can state it in a way that isn't so particular: We need
+; round-tripping of the brackets we use. Not every operation will use
+; all the round-tripping data, and that data will rarely be a core
+; motivation in the design of a high-dimensional syntax transformer,
+; but it is data almost all of those designs will have minor uses for.
+
+
+
+(define/contract (improper-list->list-and-tail lst)
+  (-> any/c #/list/c list? any/c)
+  (w-loop next rev-elems (list) tail lst
+    (expect tail (cons elem tail) (list (reverse rev-elems) tail)
+    #/next (cons elem rev-elems) tail)))
+
+(define/contract (syntax-local-maybe identifier)
+  (-> any/c maybe?)
+  (if (identifier? identifier)
+    (w- dummy (box #/trivial)
+    #/w- local (syntax-local-value identifier #/fn dummy)
+    #/if (eq? local dummy)
+      (nothing)
+      (just local))
+    (nothing)))
+
+
+
+; This struct property indicates a syntax's behavior as the kind of
+; macro expected by `s-expr-stx->ht-expr`.
+(define-values
+  (prop:ht-builder-syntax ht-builder-syntax? ht-builder-syntax-ref)
+  (make-struct-type-property 'ht-builder-syntax))
+
+(define/contract (ht-builder-syntax-maybe x)
+  (-> any/c maybe?)
+  (if (ht-builder-syntax? x)
+    (just #/ht-builder-syntax-ref x)
+    (nothing)))
+
+(define (omega-ht . closing-brackets)
+  (degree-and-closing-brackets->hypertee (onum-omega)
+    closing-brackets))
+
+(define (omega-ht-append0 hts)
+  ; When we call this, the elements of `hts` are degree-omega
+  ; hypertees, and their degree-0 holes have trivial values as
+  ; contents. We return their degree-0 concatenation.
+  (list-foldr hts (omega-ht #/list 0 #/trivial) #/fn ht tail
+    (hypertee-bind-one-degree ht 0 #/fn hole data
+      (dissect data (trivial)
+        tail))))
+
+(struct-easy (ht-tag-1-s-expr-stx stx) #:equal)
+(struct-easy (ht-tag-1-other val) #:equal)
+(struct-easy (ht-tag-2-list stx-example) #:equal)
+(struct-easy (ht-tag-2-list* stx-example) #:equal)
+(struct-easy (ht-tag-2-vector stx-example) #:equal)
+(struct-easy (ht-tag-2-prefab key stx-example) #:equal)
+(struct-easy (ht-tag-2-other val) #:equal)
+
+; This recursively converts the given Racket syntax object into an
+; degree-omega hypertee. It performs a kind of macroexpansion on lists
+; that begin with an identifier with an appropriate
+; `syntax-local-value` binding. For everything else, it uses
+; particular data structures in the holes of the result hypertee to
+; represent the other atoms, proper lists, improper lists, vectors,
+; and prefab structs it encounters.
+;
+(define/contract (s-expr-stx->ht-expr stx)
+  (-> syntax? hypertee?)
+  (mat
+    (syntax-parse stx
+      [ (op:id arg ...)
+        (maybe-bind (syntax-local-maybe #'op) #/fn op
+        #/maybe-map (ht-builder-syntax-maybe op) #/fn proc
+        #/list op proc)]
+      [op:id
+        (maybe-bind (syntax-local-maybe #'op) #/fn op
+        #/maybe-map (ht-builder-syntax-maybe op) #/fn proc
+        #/list op proc)]
+      [_ (nothing)])
+    (just #/list op proc)
+    
+    ; If `stx` is shaped like `op` or `(op arg ...)` where `op` is
+    ; bound to an `ht-builder-syntax?` syntax transformer according to
+    ; `syntax-local-value`, then we invoke it on `stx`.
+    ;
+    ; TODO: See if we can call this more like a Racket syntax
+    ; transformer. If we can, we'll want to approximate this process:
+    ;
+    ;   - Disarm `stx`.
+    ;
+    ;   - Remove any `'taint-mode` and `'certify-mode` syntax
+    ;     properties from `stx`.
+    ;
+    ;   - Rearm the result, and apply syntax properties to the result
+    ;     that correspond to the syntax properties of `stx`. It's not
+    ;     really clear how this would be performed, since the result
+    ;     is an ht-expression that may "contain" several encoded
+    ;     Racket syntax objects (their trees encoded as concentric
+    ;     degree-2 holes and their leaves as degree-1 holes) that are
+    ;     peers of each other, as well as several holes that have
+    ;     nothing to do with this encoding of Racket syntax objects.
+    ;
+    ; TODO: Use a contract to enforce that `proc` returns a single
+    ; value matching `hypertee?`. Currently, if it doesn't, then
+    ; `s-expr-stx->ht-expr` reports that it has broken its own
+    ; contract.
+    ;
+    (proc op stx)
+  #/w- process-list
+    (fn elems
+      (list-map elems #/fn elem #/s-expr-stx->ht-expr elem))
+  ; NOTE: We go to some trouble to detect improper lists here. This is
+  ; so we can preserve the metadata of syntax objects occurring in
+  ; tail positions partway through the list, which we would lose track
+  ; of if we simply used `syntax->list` or `syntax-parse` with a
+  ; pattern of `(elem ...)` or `(elem ... . tail)`.
+  #/w- s (syntax-e stx)
+  #/w- stx-example (datum->syntax stx #/list)
+  #/w- make-list-layer
+    (fn metadata elems
+      ; When we call this, `elems` is a list of degree-omega
+      ; hypertees, and their degree-0 holes have trivial contents. We
+      ; degree-0-concatenate them, and then we degree-1-concatenate a
+      ; degree-2 hole around that, holding the given metadata. We
+      ; return the degree-omega hypertee that results.
+      (hypertee-bind-one-degree
+        (omega-ht
+          (list 2 metadata)
+            1 (list 1 #/trivial) 0 0
+          0
+        #/list 0 #/trivial)
+        1
+      #/fn hole data
+        (omega-ht-append0 elems)))
+  
+  ; We traverse into proper and improper lists.
+  #/if (pair? s)
+    (dissect (improper-list->list-and-tail s) (list elems tail)
+    #/w- elems (process-list elems)
+    #/mat tail (list)
+      ; The metadata we pass in here represents the `list` operation,
+      ; so its data contains the metadata of `stx` so that clients
+      ; processing this hypertee-based encoding of this Racket syntax
+      ; can recover this layer of information about it.
+      (make-list-layer (ht-tag-2-list stx-example) elems)
+    ; NOTE: Even though we call the full `s-expr-stx->ht-expr`
+    ; operation here, we already know `#'tail` can't be cons-shaped.
+    ; Usually it'll be wrapped up as an atom. However, it could still
+    ; be expanded as a identifier syntax or processed as a vector or
+    ; as a prefab struct.
+    #/w- tail (s-expr-stx->ht-expr tail)
+      ; This is like the proper list case, but this time the metadata
+      ; represents an improper list operation (`list*`) rather than a
+      ; proper list operation (`list`).
+      (make-list-layer (ht-tag-2-list* stx-example)
+      #/append elems #/list tail))
+  
+  ; We traverse into prefab structs.
+  #/w- key (prefab-struct-key s)
+  #/if key
+    (make-list-layer (ht-tag-2-prefab key stx-example)
+    #/process-list #/cdr #/vector->list #/struct->vector s)
+  
+  #/syntax-parse stx
+    ; We traverse into vectors.
+    [ #(elem ...)
+      (w- elems (process-list #/syntax->list #'(elem ...))
+        ; This is like the proper list case, but this time the
+        ; metadata represents a vector operation (`vector`) rather
+        ; than a proper list operation (`list`).
+        (make-list-layer (ht-tag-2-vector stx-example) elems))]
+    
+    [_
+      ; We return a degree-omega hypertee with trivial contents in its
+      ; degree-0 hole, and with a single degree-1 hole that contains
+      ; `stx` itself (perhaps put in some kind of container so that it
+      ; can be distinguished from degree-1 holes that a user-defined
+      ; syntax introduces for a different reason).
+      (omega-ht (list 1 #/ht-tag-1-s-expr-stx stx) 0
+      #/list 0 #/trivial)]))
+
+; This recursively converts the given Racket syntax object into an
+; degree-omega hypertee just like `s-expr-stx->ht-expr`, but it
+; expects the outermost layer of the syntax object to be a proper
+; list, and it does not represent that list in the result, so the
+; result will "splice" all the list's elements in whatever place it's
+; inserted.
+;
+; TODO: See if we'll ever use this. Right now it's just here as a
+; reminder that ht-expressions aren't quite "expressions" so much as
+; snippets of expression-like data.
+;
+(define/contract (splicing-s-expr-stx->ht-expr stx)
+  (-> syntax? hypertee?)
+  (omega-ht-append0 #/list-map (syntax->list stx) #/fn elem
+    (s-expr-stx->ht-expr elem)))
+
+
+(struct-easy (simple-ht-builder-syntax impl)
+  #:other
+  #:property prop:ht-builder-syntax
+  (fn this stx
+    (expect this (simple-ht-builder-syntax impl)
+      (error "Expected this to be a simple-ht-builder-syntax")
+    #/impl stx)))
+
+(struct-easy
+  (syntax-and-ht-builder-syntax syntax-impl ht-builder-syntax-impl)
+  #:other
+  
+  #:property prop:procedure
+  (fn this stx
+    (expect this
+      (syntax-and-ht-builder-syntax
+        syntax-impl ht-builder-syntax-impl)
+      (error "Expected this to be a syntax-and-ht-builder-syntax")
+    #/syntax-impl stx))
+  
+  #:property prop:ht-builder-syntax
+  (fn this stx
+    (expect this
+      (syntax-and-ht-builder-syntax
+        syntax-impl ht-builder-syntax-impl)
+      (error "Expected this to be a syntax-and-ht-builder-syntax")
+    #/ht-builder-syntax-impl stx))
+)
---
+++ punctaffy-lib/private/experimental/macro/hypernest-qq.rkt
@@ -0,0 +1,306 @@
+#lang parendown racket/base
+
+; qq.rkt
+;
+; A quasiquotation operator which allows for user-defined escape
+; sequences which can generalize unquotation and nested
+; quasiquotation.
+
+;   Copyright 2018 The Lathe Authors
+;
+;   Licensed under the Apache License, Version 2.0 (the "License");
+;   you may not use this file except in compliance with the License.
+;   You may obtain a copy of the License at
+;
+;       http://www.apache.org/licenses/LICENSE-2.0
+;
+;   Unless required by applicable law or agreed to in writing,
+;   software distributed under the License is distributed on an
+;   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+;   either express or implied. See the License for the specific
+;   language governing permissions and limitations under the License.
+
+
+(require #/for-syntax racket/base)
+(require #/for-syntax #/only-in syntax/parse id syntax-parse)
+
+(provide my-quasiquote)
+
+
+
+(module part1-private racket/base
+  
+  (require #/for-template racket/base)
+  
+  (require #/only-in racket/contract/base -> any/c list/c)
+  (require #/only-in racket/contract/region define/contract)
+  (require #/only-in syntax/parse expr id syntax-parse)
+  
+  (require #/only-in lathe-comforts dissect expect fn mat w-)
+  (require #/only-in lathe-comforts/list list-foldr)
+  (require #/only-in lathe-comforts/maybe just maybe/c nothing)
+  (require #/only-in lathe-comforts/struct struct-easy)
+  (require #/only-in lathe-comforts/trivial trivial)
+  (require #/only-in lathe-ordinals onum<? onum-omega)
+  
+  (require #/only-in
+    punctaffy/private/experimental/macro/hypertee-macro
+    
+    ht-tag-1-other ht-tag-1-s-expr-stx ht-tag-2-list ht-tag-2-list*
+    ht-tag-2-other ht-tag-2-prefab ht-tag-2-vector
+    s-expr-stx->ht-expr simple-ht-builder-syntax)
+  (require #/only-in punctaffy/hypersnippet/hypertee
+    degree-and-closing-brackets->hypertee hypertee?
+    hypertee-bind-one-degree hypertee-degree hypertee-drop1
+    hypertee-fold hypertee-join-one-degree hypertee-map-one-degree
+    hypertee-promote hypertee-pure hypertee-truncate
+    hypertee-zip-selective)
+  
+  (provide #/all-defined-out)
+  
+  
+  (define (omega-ht . closing-brackets)
+    (degree-and-closing-brackets->hypertee (onum-omega)
+      closing-brackets))
+  
+  (define (omega-ht-append0 hts)
+    ; When we call this, the elements of `hts` are degree-omega
+    ; hypertees, and their degree-0 holes have trivial values as
+    ; contents. We return their degree-0 concatenation.
+    (list-foldr hts (omega-ht #/list 0 #/trivial) #/fn ht tail
+      (hypertee-bind-one-degree ht 0 #/fn hole data
+        (dissect data (trivial)
+          tail))))
+  
+  
+  (struct-easy (my-quasiquote-tag-1-expanded-expr stx) #:equal)
+  (struct-easy
+    (my-quasiquote-tag-1-unmatched-unquote
+      closing-bracket interpolation)
+    #:equal)
+  (struct-easy
+    (my-quasiquote-tag-2-matched-internal-quasiquotation
+      opening-bracket body-with-closing-brackets)
+    #:equal)
+  (struct-easy (bracket-tag-1-end) #:equal)
+  (struct-easy
+    (internal-quasiquotation-tag-1-matched-unquote closing-bracket)
+    #:equal)
+  
+  (define (make-op-bracket call-stx)
+    (syntax-parse call-stx #/ (op body)
+    #/omega-ht
+      (list 2 #/ht-tag-2-list #/datum->syntax call-stx #/list)
+      1
+      (list 1 #/ht-tag-1-s-expr-stx #'op)
+      0
+      (list 1 #/ht-tag-1-other #/bracket-tag-1-end)
+      0
+      0
+      0
+    #/list 0 #/trivial))
+  
+  (define my-quasiquote-uq
+    (simple-ht-builder-syntax #/fn stx
+      (syntax-parse stx
+        [op:id
+          ; If this syntax transformer is used in an identifier
+          ; position, we just expand as though the identifier isn't
+          ; bound to a syntax transformer at all. In this case, that
+          ; makes it possible to nest `(my-quasiquote uq #/qq ...)`
+          ; within itself, without the inner `uq` being treated as a
+          ; closing bracket.
+          (omega-ht (list 1 #/ht-tag-1-s-expr-stx stx) 0
+          #/list 0 #/trivial)]
+      #/ (op interpolation)
+      #/omega-ht
+        (list 1
+        #/ht-tag-1-other #/my-quasiquote-tag-1-unmatched-unquote
+          (make-op-bracket stx)
+          #'interpolation)
+        0
+      #/list 0 #/trivial)))
+  
+  (define my-quasiquote-qq
+    (simple-ht-builder-syntax #/fn stx
+      (syntax-parse stx
+        [op:id
+          ; If this syntax transformer is used in an identifier
+          ; position, we just expand as though the identifier isn't
+          ; bound to a syntax transformer at all.
+          (omega-ht (list 1 #/ht-tag-1-s-expr-stx stx) 0
+          #/list 0 #/trivial)]
+      #/ (op body)
+      #/w- body (s-expr-stx->ht-expr #'body)
+      ; We make a hypertee with a single degree-2 hole (annotated with
+      ; the body of the quasiquotation) and some degree-1 holes
+      ; (annotated with the `s-expr-stx->ht-expr` expansions of the
+      ; interpolations) in its degree-1 holes.
+      #/w- intermediate
+        (hypertee-pure (onum-omega)
+          (ht-tag-2-other #/my-quasiquote-tag-2-matched-internal-quasiquotation
+            (make-op-bracket stx)
+            (hypertee-map-one-degree body 1 #/fn hole data
+              (expect data
+                (ht-tag-1-other #/my-quasiquote-tag-1-unmatched-unquote
+                  closing-bracket interpolation)
+                data
+              #/ht-tag-1-other #/internal-quasiquotation-tag-1-matched-unquote
+                closing-bracket)))
+          (hypertee-truncate 2
+          #/hypertee-bind-one-degree body 1 #/fn hole data
+            (expect data
+              (ht-tag-1-other #/my-quasiquote-tag-1-unmatched-unquote
+                closing-bracket interpolation)
+              (hypertee-promote (onum-omega) hole)
+            #/hypertee-pure (onum-omega)
+              (s-expr-stx->ht-expr interpolation)
+              hole)))
+      ; We join on those degree-1 holes so that those expansions
+      ; become part of this expansion directly.
+      #/hypertee-join-one-degree intermediate 1)))
+  
+  (define (my-quasiquote-ht-expr->stx ht-expr)
+    (expect
+      (hypertee-fold 1 ht-expr #/fn first-nontrivial-d data tails
+        (w- d (hypertee-degree tails)
+        #/w- is-trivial (onum<? d first-nontrivial-d)
+        #/begin
+          (when is-trivial
+            (dissect data (trivial)
+            #/void))
+        #/mat d 0
+          (expect is-trivial #t
+            (error "Encountered an unrecognized degree-0 hole in quasiquoted syntax")
+          #/list)
+        #/expect is-trivial #f
+          ; Since `first-nontrivial-d` can only exceed 1 when we're in
+          ; a degree-2-or-greater hole of a degree-3-or-greater hole,
+          ; and since we expect no degree-3-or-greater holes, we raise
+          ; an error.
+          (error "Encountered unexpectedly high-dimensional structure in quasiquoted syntax")
+        #/mat d 1
+          (dissect (hypertee-drop1 tails) (just #/list rest tails)
+          #/dissect (hypertee-drop1 tails) (nothing)
+          #/(fn result #/cons result rest)
+          #/mat data (ht-tag-1-s-expr-stx stx) #`'#,stx
+          #/mat data
+            (ht-tag-1-other #/my-quasiquote-tag-1-expanded-expr stx)
+            stx
+          #/mat data
+            (ht-tag-1-other #/my-quasiquote-tag-1-unmatched-unquote
+              bracket-ht-expr stx)
+            (syntax-parse stx
+              [interpolation:expr stx]
+              [_  (error "Encountered an interpolation of a non-expression in quasiquoted syntax")])
+          #/error "Encountered an unrecognized degree-1 hole in quasiquoted syntax")
+        #/mat d 2
+          (dissect
+            (hypertee-fold 1 tails #/fn first-nontrivial-d data tails
+              (w- d (hypertee-degree tails)
+              #/mat d 0 (list (list) data)
+              #/dissect d 1
+              #/dissect (hypertee-drop1 tails)
+                (just #/list (list elems rest) tails)
+              #/dissect (hypertee-drop1 tails) (nothing)
+              #/list (append data elems) rest))
+            (list elems rest)
+          #/(fn result #/cons result rest)
+          #/mat data (ht-tag-2-list stx-example) #`(list #,@elems)
+          #/mat data (ht-tag-2-list* stx-example) #`(list* #,@elems)
+          #/mat data (ht-tag-2-vector stx-example) #`(vector #,@elems)
+          #/mat data (ht-tag-2-prefab key stx-example)
+            ; NOTE: The expression this generates can raise an error
+            ; if the struct has more fields than prefab structs allow.
+            #`(make-prefab-struct '#,key #,@elems)
+          
+          ; This is the support for nested quasiquotation. We use the
+          ; `opening-bracket` and `body-with-closing-brackets` to
+          ; generate an expression that generates the same data that
+          ; was parsed to make the opening bracket, body, and closing
+          ; brackets in the first place.
+          ;
+          #/mat data
+            (ht-tag-2-other #/my-quasiquote-tag-2-matched-internal-quasiquotation
+              opening-bracket body-with-closing-brackets)
+            (w- body-as-closing-bracket
+              (hypertee-bind-one-degree body-with-closing-brackets 1
+              #/fn hole data
+                (mat data
+                  (ht-tag-1-other #/internal-quasiquotation-tag-1-matched-unquote
+                    closing-bracket)
+                  closing-bracket
+                #/hypertee-pure (onum-omega) data hole))
+            #/w- zip-bracket-ends
+              (fn smaller bigger func
+                (hypertee-zip-selective smaller bigger
+                  (fn hole data
+                    (w- d (hypertee-degree hole)
+                    #/mat d 0
+                      (dissect data (trivial)
+                        #t)
+                    #/mat d 1
+                      (mat data (ht-tag-1-other #/bracket-tag-1-end)
+                        #t
+                        #f)
+                      #f))
+                #/fn hole smaller-data bigger-data
+                  (w- d (hypertee-degree hole)
+                  #/mat d 0
+                    (dissect smaller-data (trivial)
+                    #/dissect bigger-data (trivial)
+                    #/trivial)
+                  #/mat d 1
+                    (dissect bigger-data
+                      (ht-tag-1-other #/bracket-tag-1-end)
+                    #/func smaller-data)
+                  #/error "Internal error: Encountered unexpectedly high-dimensional structure when zipping bracket ends")))
+            #/expect
+              (zip-bracket-ends
+                (hypertee-map-one-degree tails 0 #/fn hole data
+                  (trivial))
+                body-as-closing-bracket
+              #/fn tails-data
+                (expect tails-data (list stx)
+                  ; TODO: See if this is really an "internal" error or
+                  ; if there's some input that can legitimately cause
+                  ; this error.
+                  (error "Internal error: Somehow reconstructed more than one syntax object out of a nested quasiquotation's unquoted syntax")
+                #/ht-tag-1-other #/my-quasiquote-tag-1-expanded-expr
+                  stx))
+              (just body-and-tails)
+              (error "Internal error: Expected tails to have the same shape as the nested quasiquotation's body and closing bracket data combined")
+            #/w- body-and-tails
+              (my-quasiquote-ht-expr->stx body-and-tails)
+            #/expect
+              (zip-bracket-ends
+                (omega-ht (list 1 body-and-tails) 0
+                #/list 0 #/trivial)
+                opening-bracket
+              #/fn body-and-tails
+                (ht-tag-1-other #/my-quasiquote-tag-1-expanded-expr
+                  body-and-tails))
+              (just body-and-tails)
+              (error "Internal error: Expected opening-bracket to have exactly one end")
+            #/my-quasiquote-ht-expr->stx body-and-tails)
+          
+          #/error "Encountered an unrecognized degree-2 hole in quasiquoted syntax")
+        #/error "Encountered unexpectedly high-dimensional structure in quasiquoted syntax"))
+      (list stx)
+      (error "Internal error: Somehow reconstructed more than one syntax object out of quasiquoted syntax")
+      stx))
+  
+  (define (my-quasiquote-begin-fn stx)
+    (syntax-parse stx #/ (_ quotation:expr)
+    #/my-quasiquote-ht-expr->stx #/s-expr-stx->ht-expr #'quotation))
+  
+)
+(require #/for-syntax 'part1-private)
+
+
+(define-syntax my-quasiquote-begin my-quasiquote-begin-fn)
+
+(define-syntax (my-quasiquote stx)
+  (syntax-parse stx #/ (_ uq:id (qq:id body))
+    #'(let-syntax ([uq my-quasiquote-uq] [qq my-quasiquote-qq])
+      #/my-quasiquote-begin body)))
