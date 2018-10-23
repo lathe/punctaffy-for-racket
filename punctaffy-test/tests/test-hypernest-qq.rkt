024215f3cc2fdb07b0cd400127acf13bc3689bfc
Ross Angle <rokitna@hotmail.com>
2018-10-23 15:42:15 -0700

    COMPLETE: The test-hypernest-qq.rkt tests actually work; we just needed to be patient and run three of them for 15-20m each. Update the comments about how long they take with this information. Revise the TODO comments to account for the fact we are now successfully using an ADT-style representation of hypernests.

--- punctaffy-test/tests/test-hypernest-qq.rkt
+++ punctaffy-test/tests/test-hypernest-qq.rkt
@@ -42,47 +42,39 @@
 ; `hn-tag-nest` region without being confused with closing brackets
 ; bounding the region.
 ;
-; To illustrate in more detail, the problem we avoid that way is that
-; if we have one `hn-tag-nest` representing a degree-2 snippet of the
-; code (encoded as a degree-4 bump) and inside its degree-2 region we
-; have a degree-1 snippet of code represented as a degree-1 bump, then
-; the first hypertee in the representation of this hypernest will have
-; a degree-1 hole (the inner bump) inside a degree-2 region, which is
-; treated (whether we like it or not) as a hole in that region rather
-; than as a bump. Bumping all the degree-1 snippets up to degree 3
-; lets us avoid this.
+; The problem we avoided that way has to do with a previous
+; representation of hypernests we were using. That representation
+; treated all the hypernest's immediate holes and bumps as being holes
+; of a hypertee, and it treated the interior of each bump as another
+; hypertee. This representation was incorrect because it wasn't able
+; to represent a low-degree bump that occurred inside of a high-degree
+; hole of a higher-degree hole or bump. Those low-degree bumps would
+; have needed to be represented by low-degree holes, but the
+; low-degree holes occurring there were inside high-degree holes in
+; the hypertee representation, so they could only represent holes in
+; the high-degree hole, not bumps in it.
 ;
-; We shouldn't really have to think about that when we're using
-; hypernests, so there's probably a representation format that fits
-; the hypernest format better. Here's an approach that could work:
-; Instead of representing `hn-tag-nest` as contour-of-contour-shaped
-; bumps with empty interiors, we could represent them with bumps whose
-; interiors correspond to the interiors of the brackets they represent
-; and whose data annotations contain are appropriately shaped
-; hypernest values which encode the brackets' original syntax.
+; To work around this problem at first, we represented expressions
+; using degree-3 regions and list-like operations using degree-4
+; regions. This put them at just high enough a degree that we could
+; get these quasiquotation tests to work. I believe an operator of
+; higher degree than quasiquotation would have required us to raise
+; the degrees of our regions accordingly, so this was not an approach
+; that allowed us to express operators of arbitrary degree.
 ;
-; On the other hand, since we have been able to get this working with
-; that degree-3 approach, do we really need hypernests at all?
+; Now, we have changed the representation of hypernests so that it
+; *is* possible to represent a low-degree bump beyond a higher-degree
+; hole in a high-degree hole or bump. This means we should be able to
+; switch the expressions and list-like operators back to using
+; degree-1 and degree-2 regions. If we can, I don't believe we will
+; have trouble implementing operations of higher degree than
+; quasiquotation.
 
 
-; TODO NOW: Some of these tests haven't succeeded ever since we
-; changed the implementation of hypernests to an AST-style approach
-; (which was necessary so we could have bumps of low degree in the
-; areas beyond the low-degree holes of bumps and holes of high
-; degree). Fix these tests. Then go back and change `hn-tag-3-...` and
-; `hn-tag-4-...` to `hn-tag-1-...` and `hn-tag-2...` since those
-; lower-degree encodings will have become correct.
-;
-; Note that technically `hn-tag-3-...` and `hn-tag-4-...` were always
-; of too low degree -- they're only of degree high enough to get
-; quasiquote to work -- which is why we needed to start fixing the way
-; we were representing hypernests.
-
-
-; Altogether, these commented-out tests seem to take about (TODO: Time
-; these once we're sure they terminate.) to run (on my machine). They
-; take about 28.7s if `assert-valid-hypertee-brackets` and
-; `assert-valid-hypernest-coil` are no-ops.
+; Altogether, these commented-out tests seem to take about 54m52s to
+; run (on my machine). They take about 28.7s if
+; `assert-valid-hypertee-brackets` and `assert-valid-hypernest-coil`
+; are no-ops.
 ;
 ; Each one is labeled with the amount of time it takes to run
 ; individually (with and without those `assert-...` passes).
@@ -106,14 +98,8 @@
         ,(+ 4 5)))
   "The new quasiquote works a lot like the original")
 
-; Time with assertions:      >5m
+; Time with assertions:      20m54s
 ; Time without assertions:      13.3s
-;
-; An earlier version of the code took over 16m with assertions, and
-; this one is probably no different.
-;
-; TODO NOW: See if there's an infinite loop when the assertions are
-; active.
 ;
 #;
 (check-equal?
@@ -133,11 +119,8 @@
               ,(+ 4 5))))))
   "The new quasiquote works on data that looks roughly similar to nesting")
 
-; Time with assertions:      >5m
+; Time with assertions:      14m01s
 ; Time without assertions:      20.1s
-;
-; TODO NOW: See if there's an infinite loop when the assertions are
-; active.
 ;
 #;
 (check-equal?
@@ -157,11 +140,8 @@
               ,(+ 4 5))))))
   "The new quasiquote supports nesting")
 
-; Time with assertions:      >5m
+; Time with assertions:      15m10s
 ; Time without assertions:      16.6s
-;
-; TODO NOW: See if there's an infinite loop when the assertions are
-; active.
 ;
 #;
 (check-equal?
