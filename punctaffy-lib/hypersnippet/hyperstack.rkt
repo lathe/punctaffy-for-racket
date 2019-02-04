7a6f76ffba320829eeaa1aa57a32cfe62f6a111a
Ross Angle <rokitna@hotmail.com>
2019-02-04 05:57:21 -0800

    Start to convert hyperstack.rkt to use a more generalized notion of dimension numbers so that Punctaffy won't have to depend on Lathe Ordinals. This lets us work on making Punctaffy stable and well documented before Lathe Ordinals is stable and well documented, and better yet, it means the concept of ordinal-sized dimensions won't raise the nagging question of why don't we have ordinal-sized hypersnippets. So far, we have basically a complete reimplementation of hyperstack.rkt, but we have the old hyperstack.rkt code in there because other files depend on it.

--- punctaffy-lib/hypersnippet/hyperstack.rkt
+++ punctaffy-lib/hypersnippet/hyperstack.rkt
@@ -20,12 +20,17 @@
 ;   language governing permissions and limitations under the License.
 
 
-(require #/only-in racket/contract/base -> ->i any/c list/c or/c)
+(require #/only-in racket/contract struct-type-property/c)
+(require #/only-in racket/contract/base
+  -> ->i any/c contract? contract-out list/c listof or/c
+  rename-contract)
 (require #/only-in racket/contract/region define/contract)
 
 (require #/only-in lathe-comforts dissect dissectfn expect fn mat w-)
 (require #/only-in lathe-comforts/maybe just)
-(require #/only-in lathe-comforts/struct struct-easy)
+(require #/only-in lathe-comforts/struct
+  auto-write define-imitation-simple-generics
+  define-imitation-simple-struct struct-easy)
 (require #/only-in lathe-comforts/trivial trivial)
 (require #/only-in lathe-ordinals onum<? onum</c onum-drop onum<=e0?)
 (require #/only-in lathe-ordinals/olist
@@ -33,6 +38,152 @@
   olist-tails olist-plus olist-ref-and-call olist-zip-map olist-zero)
 
 ; TODO: Document all of these exports.
+(provide #/contract-out
+  
+  ; TODO: See if the `dym-sys?` utilities should be provided by
+  ; another module.
+  [dim-sys? (-> any/c boolean?)]
+  [dim-sys-impl? (-> any/c boolean?)]
+  [dim-sys-dim/c (-> dim-sys? contract?)]
+  [dim-sys-dim=?
+    (->i
+      (
+        [ds dim-sys?]
+        [a (ds) (dim-sys-dim/c ds)]
+        [b (ds) (dim-sys-dim/c ds)])
+      [_ boolean?])]
+  [dim-sys-dim</c
+    (->i ([ds dim-sys?] [bound (ds) (dim-sys-dim/c ds)])
+      [_ contract?])]
+  [dim-sys-accepts? (-> dim-sys? dim-sys? boolean?)]
+  [dim-sys-dimlist/c (-> dim-sys? contract?)]
+  [dim-sys-dimlist-build
+    (->i
+      (
+        [ds dim-sys?]
+        [length (ds) (dim-sys-dim/c ds)]
+        [func (ds) (-> (dim-sys-dim/c ds) any/c)])
+      [_ (ds) (dim-sys-dimlist/c ds)])]
+  [dim-sys-dimlist-ref-and-call
+    (->i
+      (
+        [ds dim-sys?]
+        [lst (ds) (dim-sys-dimlist/c ds)]
+        [i (ds) (dim-sys-dim/c ds)])
+      [_ any/c])]
+  [dim-sys-dimlist-length
+    (->i ([ds dim-sys?] [lst (ds) (dim-sys-dimlist/c ds)])
+      [_ (ds) (dim-sys-dim/c ds)])]
+  [dim-sys-dimlist-map
+    (->i
+      (
+        [ds dim-sys?]
+        [lst (ds) (dim-sys-dimlist/c ds)]
+        [v->v (-> any/c any/c)])
+      [_ (ds) (dim-sys-dimlist/c ds)])]
+  [dim-sys-dimlist-zip-map
+    (->i
+      (
+        [ds dim-sys?]
+        [a (ds) (dim-sys-dimlist/c ds)]
+        [b (ds) (dim-sys-dimlist/c ds)]
+        [func (-> any/c any/c any/c)])
+      
+      #:pre (ds a b)
+      (dim-sys-dim=? ds
+        (dim-sys-dimlist-length ds a)
+        (dim-sys-dimlist-length ds b))
+      
+      [_ (ds) (dim-sys-dimlist/c ds)])]
+  [dim-sys-dimlist-zero
+    (->i ([ds dim-sys?]) [_ (ds) (dim-sys-dimlist/c ds)])]
+  [dim-sys-dimlist-chevrons
+    ; TODO: See if we should make this contract more specific. The
+    ; result value is a dimension-indexed list of dimension-indexed
+    ; lists.
+    (->i ([ds dim-sys?] [lst (ds) (dim-sys-dimlist/c ds)])
+      [_ (ds) (dim-sys-dimlist/c ds)])]
+  [prop:dim-sys (struct-type-property/c dim-sys-impl?)]
+  [make-dim-sys-impl-from-max
+    (->i
+      (
+        [dim/c (-> dim-sys? contract?)]
+        [dim-max-of-list (dim/c)
+          (->i ([ds dim-sys?] [lsts (ds) (listof #/dim/c ds)])
+            [_ (ds) (dim/c ds)])])
+      [_ dim-sys-impl?])]
+  
+  [hyperstack? (-> any/c boolean?)]
+  [hyperstack/c (-> dim-sys? contract?)]
+  [hyperstack-dim-sys (-> hyperstack? dim-sys?)]
+  [make-hyperstack
+    (->i ([ds dim-sys?] [elems (ds) (dim-sys-dim/c ds)])
+      [_ (ds) (hyperstack/c ds)])]
+  [hyperstack-dimension
+    (->i ([h hyperstack?])
+      [_ (h) (dim-sys-dim/c #/hyperstack-dim-sys h)])]
+  [hyperstack-peek-elem
+    (->i
+      (
+        [h hyperstack?]
+        [i (h)
+          (w- ds (hyperstack-dim-sys h)
+          #/dim-sys-dim</c ds #/hyperstack-dimension h)])
+      [_ any/c])]
+  [hyperstack-push
+    (->i
+      (
+        [h hyperstack?]
+        [elems-to-push (h)
+          (dim-sys-dimlist/c #/hyperstack-dim-sys h)])
+      [_ (h) (hyperstack/c #/hyperstack-dim-sys h)])]
+  [hyperstack-pop
+    (->i
+      (
+        [h hyperstack?]
+        [elems-to-push (h)
+          (dim-sys-dimlist/c #/hyperstack-dim-sys h)])
+      
+      #:pre (h elems-to-push)
+      (w- ds (hyperstack-dim-sys h)
+      #/dim-sys-dim<? ds
+        (dim-sys-dimlist-length ds elems-to-push)
+        (hyperstack-dimension h))
+      
+      [_ (h)
+        (list/c (or/c 'root 'push 'pop) any/c
+          (hyperstack/c #/hyperstack-dim-sys h))])]
+  
+  [make-hyperstack-n
+    (->i ([ds dim-sys?] [dimension (ds) (dim-sys-dim/c ds)])
+      [_ (ds) (hyperstack/c ds)])]
+  [hyperstack-push-uniform
+    (->i
+      (
+        [h hyperstack?]
+        [bump-degree (h) (dim-sys-dim/c #/hyperstack-dim-sys h)]
+        [elem any/c])
+      [_ (h) (hyperstack/c #/hyperstack-dim-sys h)])]
+  [hyperstack-pop-n-with-barrier
+    (->i
+      (
+        [h hyperstack?]
+        [i (h)
+          (w- ds (hyperstack-dim-sys h)
+          #/dim-sys-dim</c ds #/hyperstack-dimension h)])
+      [_ (h)
+        (list/c (or/c 'root 'push 'pop)
+          (hyperstack/c #/hyperstack-dim-sys h))])]
+  [hyperstack-pop-n
+    (->i
+      (
+        [h hyperstack?]
+        [i (h)
+          (w- ds (hyperstack-dim-sys h)
+          #/dim-sys-dim</c ds #/hyperstack-dimension h)])
+      [_ (h) (hyperstack/c #/hyperstack-dim-sys h)])]
+  
+  )
 (provide
   make-poppable-hyperstack
   poppable-hyperstack-dimension
@@ -49,6 +200,186 @@
   pushable-hyperstack-pop
   
   pushable-hyperstack-push-uniform)
+
+
+(define-imitation-simple-generics dim-sys? dim-sys-impl?
+  (#:method dim-sys-dim/c (#:this))
+  (#:method dim-sys-dim-max-of-list (#:this) ())
+  prop:dim-sys make-dim-sys-impl-from-max
+  'dim-sys 'dim-sys-impl (list))
+
+(define (dim-sys-dim-max ds . args)
+  (dim-sys-dim-max-of-list ds args))
+
+(define (dim-sys-dim-zero ds)
+  (dim-sys-dim-max ds))
+
+(define (dim-sys-dim=? ds a b)
+  (equal? a b))
+
+(define (dim-sys-dim<=? ds a b)
+  (dim-sys-dim=? ds b #/dim-sys-dim-max ds a b))
+
+(define (dim-sys-dim<? ds a b)
+  (and (not #/dim-sys-dim=? ds a b) (dim-sys-dim<=? ds a b)))
+
+(define (dim-sys-dim</c ds bound)
+  (rename-contract (fn v #/dim-sys-dim<? ds v bound)
+    `(dim-sys-dim</c ,ds ,bound)))
+
+(define (dim-sys-accepts? ds other)
+  (equal? ds other))
+
+(define-imitation-simple-struct
+  (dimlist? dimlist-dim-sys dimlist-length dimlist-func)
+  dimlist 'dimlist (current-inspector) (auto-write))
+
+(define (dim-sys-dimlist? ds v)
+  (and (dimlist? v) (dim-sys-accepts? ds #/dimlist-dim-sys v)))
+
+(define (dim-sys-dimlist/c ds)
+  (rename-contract (fn v #/dim-sys-dimlist? ds v)
+    `(dim-sys-dimlist/c ,ds)))
+
+(define (dim-sys-dimlist-build ds length func)
+  (dimlist ds length func))
+
+(define (dim-sys-dimlist-uniform ds length elem)
+  (dim-sys-dimlist-build ds length #/dissectfn _ elem))
+
+(define (dim-sys-dimlist-ref-and-call ds lst i)
+  (dissect lst (dimlist ds length func)
+  #/func i))
+
+(define (dim-sys-dimlist-length ds lst)
+  (dimlist-length lst))
+
+(define (dim-sys-dimlist-map ds lst v->v)
+  (dissect lst (dimlist ds length func)
+  #/dimlist ds length #/fn i #/v->v #/func i))
+
+(define (dim-sys-dimlist-zip-map ds a b func)
+  (dissect a (dimlist _ length a-func)
+  #/dissect b (dimlist _ _ b-func)
+  #/dimlist ds length #/fn i #/func (a-func i) (b-func i)))
+
+(define (dim-sys-dimlist-shadow ds shadower orig)
+  (dissect shadower (dimlist _ shadower-length shadower-func)
+  #/dissect orig (dimlist _ orig-length orig-func)
+  #/if (dim-sys-dim<=? ds orig-length shadower-length) shadower
+  #/dimlist ds (dim-sys-dim-max ds orig-length shadower-length) #/fn i
+    (if (dim-sys-dim<=? ds i shadower-length)
+      (shadower-func i)
+      (orig-func i))))
+
+(define (dim-sys-dimlist-zero ds)
+  (w- zero (dim-sys-dim-zero ds)
+  #/dim-sys-dimlist-build ds zero #/fn value-less-than-zero
+    (raise-arguments-error 'dim-sys-dimlist-zero-result
+      "a dim-sys implementation allowed for a dimension value which was less strictly less than the \"zero\" obtained as the maximum of an empty set of dimension values"
+      "zero" zero
+      "value-less-than-zero" value-less-than-zero)))
+
+; Given a dimension system and a dimension-indexed list in that
+; system, returns a list of the same length where, at each index `i`,
+; the value is the original list but possibly with the thunks at
+; indexes less than `i` changed.
+;
+; This means if a list of length `n` goes in, then what comes out is a
+; list of length `n` containing lists of length `n`, but each of those
+; inner lists has a progressively thinner stripe of indices where it's
+; well-defined. Since the indices aren't necessarily totally ordered,
+; but merely form a bounded semilattice, the stripe of values less
+; than `n` but not less than than `i` would tend to be drawn with a
+; chevron chape (a diamond with a diamond cut out of the bottom tip).
+;
+; NOTE: The behavior we implement here isn't customizable per
+; dimension system yet. It just returns lists that are exactly like
+; the input list. Someday, for certain dimension systems, customizing
+; this behavior may achieve goals related to performance or
+; implementation details. For instance, it may allow the values stored
+; in those indexes to be reclaimed by the garbage collector, or it may
+; eliminate information that would only be distracting during
+; debugging.
+;
+(define (dim-sys-dimlist-chevrons ds lst)
+  (dim-sys-dimlist-uniform ds (dim-sys-dimlist-length ds lst) lst))
+
+; TODO: See if we can simulate our uses of `onum-drop`, `olist-drop`,
+; `olist-tails`, and `olist-plus` with `dym-sys?` values.
+
+
+(define-imitation-simple-struct
+  (hyperstack? hyperstack-dim-sys hyperstack-rep)
+  hyperstack 'hyperstack (current-inspector) (auto-write))
+
+(define (hyperstack/c ds)
+  (rename-contract
+    (fn v
+      (and
+        (hyperstack? v)
+        (dim-sys-accepts? ds #/hyperstack-dim-sys v)))
+    `(hyperstack/c ,ds)))
+
+(define (make-hyperstack ds elems)
+  (hyperstack ds #/dim-sys-dimlist-map elems #/fn elem
+    (list 'root elem #/dim-sys-dimlist-zero ds)))
+
+(define (hyperstack-dimension h)
+  (dissect h (hyperstack ds rep)
+  #/dim-sys-dimlist-length ds rep))
+
+(define (hyperstack-peek-elem h i)
+  (dissect h (hyperstack ds rep)
+  #/dissect (dim-sys-dimlist-ref-and-call ds rep i)
+    (list barrier elem suspended-chevron)
+    elem))
+
+(define (hyperstack-push h elems-to-push)
+  (dissect h (hyperstack ds rep)
+  #/pushable-hyperstack #/dim-sys-dimlist-shadow ds
+    (dim-sys-dimlist-zip-map ds
+      elems-to-push
+      (dim-sys-dimlist-chevrons rep)
+      (fn elem rep-chevron
+        (list 'push elem rep-chevron)))
+    rep))
+
+(define (hyperstack-pop h elems-to-push)
+  (dissect h (hyperstack ds rep)
+  #/w- i (dim-sys-dimlist-length ds elems-to-push)
+  #/dissect (dim-sys-dimlist-ref-and-call ds rep i)
+    (list popped-barrier elem suspended-chevron)
+  #/list popped-barrier elem
+    (hyperstack ds #/dim-sys-dimlist-shadow ds
+      (dim-sys-dimlist-zip-map ds
+        elems-to-push
+        (dim-sys-dimlist-chevrons rep)
+        (fn elem rep-chevron
+          (list 'pop elem rep-chevron)))
+      (dim-sys-dimlist-map ds elems-to-push #/fn elem
+        (list 'pop elem rep))
+      suspended-chevron)))
+
+(define (make-hyperstack-n ds dimension)
+  (make-hyperstack ds
+  #/dim-sys-dimlist-uniform ds dimension #/trivial))
+
+(define (hyperstack-push-uniform h bump-degree elem)
+  (w- ds (hyperstack-dim-sys h)
+  #/hyperstack-push h #/dim-sys-dimlist-uniform ds bump-degree elem))
+
+(define (hyperstack-pop-n-with-barrier h i)
+  (w- ds (hyperstack-dim-sys h)
+  #/dissect
+    (hyperstack-pop h #/dim-sys-dimlist-uniform ds i #/trivial)
+    (list popped-barrier elem rest)
+  #/list popped-barrier rest))
+
+(define (hyperstack-pop-n h i)
+  (dissect (hyperstack-pop-n-with-barrier h i)
+    (list popped-barrier rest)
+    rest))
 
 
 (struct-easy (poppable-hyperstack nested-olist))
