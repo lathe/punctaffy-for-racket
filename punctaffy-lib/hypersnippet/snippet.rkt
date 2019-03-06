42ff1fb8220476960969deee0ff22951136e5915
Ross Angle <rokitna@hotmail.com>
2019-03-06 05:03:36 -0800

    Write a new (and untested) implementation of hypertees using an AST representation where each coil's hole shape is stored separately from its tails. Using this representation, hypertees can have an instance of `snippet-sys?` which doesn't need to do the work of creating a hole shape each time a callback needs to be invoked.

--- punctaffy-lib/hypersnippet/snippet.rkt
+++ punctaffy-lib/hypersnippet/snippet.rkt
@@ -32,7 +32,8 @@
   blame-add-context coerce-contract contract-first-order-passes?
   make-contract raise-blame-error)
 
-(require #/only-in lathe-comforts dissect dissectfn expect fn mat w-)
+(require #/only-in lathe-comforts
+  dissect dissectfn expect expectfn fn mat w- w-loop)
 (require #/only-in lathe-comforts/match match/c)
 (require #/only-in lathe-comforts/maybe
   just just? just-value maybe? maybe-bind maybe/c maybe-map nothing)
@@ -42,7 +43,8 @@
 (require #/only-in lathe-comforts/trivial trivial trivial?)
 
 (require #/only-in punctaffy/hypersnippet/dim
-  dim-sys? dim-sys-dim<? dim-sys-dim/c)
+  dim-sys? dim-sys-accepts? dim-sys-dim<? dim-sys-dim=? dim-sys-dim=0?
+  dim-sys-dim/c dim-sys-dim-max dim-sys-dim-zero)
 
 ; TODO: Document all of these exports.
 (provide
@@ -124,9 +126,11 @@
           (snippet-sys-snippet/c
             (snippet-sys-shape-snippet-sys ss)))])]
   ; TODO: See if the result contract should be more specific. The
-  ; resulting snippet should always be of the given degree, and the
-  ; result should always exist if the given degree is greater than or
-  ; equal to the degree returned by `snippet-sys-snippet-undone`.
+  ; resulting snippet should always be of the given degree, the result
+  ; should always exist if the given degree is equal to the degree
+  ; returned by `snippet-sys-snippet-undone`, and it should always
+  ; exist if the given degree is greater than that degree and that
+  ; degree is nonzero.
   [snippet-sys-snippet-set-degree-maybe
     (->i
       (
@@ -134,9 +138,10 @@
         [degree (ss) (dim-sys-dim/c #/snippet-sys-dim-sys ss)]
         [snippet (ss) (snippet-sys-snippet/c ss)])
       [_ (ss) (maybe/c #/snippet-sys-snippet/c ss)])]
-  ; TODO: See if the result contract should be more specific. The
-  ; resulting snippet should always be of the same shape as the given
-  ; shape in its low-degree holes.
+  ; TODO: See if the contract should be more specific. The given shape
+  ; should always be of a degree strictly less than the given degree,
+  ; and the resulting snippet should always be of the same shape as
+  ; the given shape in its low-degree holes.
   [snippet-sys-snippet-done
     (->i
       (
@@ -187,10 +192,7 @@
                 (and/c
                   (snippet-sys-snippetof ss #/fn hole
                     (selectable/c any/c trivial?))
-                  (snippet-sys-snippet-zip-selective/c ss
-                    (snippet-sys-snippet-select-everything
-                      (snippet-sys-shape-snippet-sys ss)
-                      hole)
+                  (snippet-sys-snippet-zip-selective/c ss hole
                     (fn hole subject-data #/selected? subject-data)
                     (fn hole shape-data subject-data any/c))))])])
       [_ (ss) (maybe/c #/snippet-sys-snippet/c ss)])]
@@ -201,7 +203,7 @@
         [shape (ss)
           (snippet-sys-snippetof
             (snippet-sys-shape-snippet-sys ss)
-            (fn hole #/selectable/c trivial? any/c))]
+            (fn hole trivial?))]
         [snippet (ss)
           (snippet-sys-snippetof ss #/fn hole selectable?)]
         [hvv-to-maybe-v (ss)
@@ -293,10 +295,7 @@
                   (and/c
                     (snippet-sys-snippetof ss #/fn hole
                       (selectable/c any/c trivial?))
-                    (snippet-sys-snippet-zip-selective/c ss
-                      (snippet-sys-snippet-select-everything
-                        (snippet-sys-shape-snippet-sys ss)
-                        hole)
+                    (snippet-sys-snippet-zip-selective/c ss hole
                       (fn hole subject-data #/selected? subject-data)
                       (fn hole shape-data subject-data any/c))))])])
         [_ (ss) (maybe/c #/snippet-sys-snippet/c ss)])
@@ -307,7 +306,7 @@
           [shape (ss)
             (snippet-sys-snippetof
               (snippet-sys-shape-snippet-sys ss)
-              (fn hole #/selectable/c trivial? any/c))]
+              (fn hole trivial?))]
           [snippet (ss)
             (snippet-sys-snippetof ss #/fn hole selectable?)]
           [hvv-to-maybe-v (ss)
@@ -635,3 +634,346 @@
             (selected result-data)))
       #/fn snippet
         (selective-snippet snippet)))))
+
+
+; TODO: Export and use the rest of these.
+
+
+(define (snippet-sys-snippet-zip-map ss shape snippet hvv-to-maybe-v)
+  (w- snippet (snippet-sys-snippet-select-everything ss snippet)
+  #/snippet-sys-snippet-zip-map-selective
+    ss shape snippet hvv-to-maybe-v))
+
+; TODO: See if this should have the question mark in its name.
+(define (snippet-sys-snippet-zip-all? ss shape snippet check-hvv?)
+  (w- snippet (snippet-sys-snippet-select-everything ss snippet)
+  #/snippet-sys-snippet-zip-all-selective?
+    ss shape snippet check-hvv?))
+
+(define (snippet-sys-shape=? ss a b hvv=?)
+  (w- shape-ss (snippet-sys-shape-snippet-sys ss)
+  #/snippet-sys-snippet-zip-all? shape-ss a b #/fn hole a-data b-data
+    (hvv=? hole a-data b-data)))
+
+(define (snippet-sys-snippet-filter-maybe ss snippet)
+  (w- ds (snippet-sys-dim-sys ss)
+  #/w- d (snippet-sys-snippet-degree ss snippet)
+  #/snippet-sys-snippet-splice ss snippet #/fn hole data
+    (mat data (selected data) (just #/unselected data)
+    #/dissect data (unselected data)
+    #/maybe-map
+      (snippet-sys-snippet-set-degree-maybe ss
+        (snippet-sys-snippet-degree ss snippet)
+        (snippet-sys-shape->snippet ss hole))
+    #/fn patch
+      (selected #/snippet-sys-snippet-select-if-degree< ss
+        (snippet-sys-snippet-degree ss hole)
+        patch))))
+
+(define (snippet-sys-snippet-map-selective ss snippet hv-to-v)
+  (snippet-sys-snippet-map ss snippet #/fn hole data
+    (mat data (unselected data) data
+    #/dissect data (selected data) (hv-to-v hole data))))
+
+
+(define-imitation-simple-struct
+  (hypertee-coil-zero?)
+  hypertee-coil-zero
+  'hypertee-coil-zero (current-inspector) (auto-write) (auto-equal))
+(define-imitation-simple-struct
+  (hypertee-coil-hole?
+    hypertee-coil-hole-overall-degree
+    hypertee-coil-hole-hole
+    hypertee-coil-hole-data
+    hypertee-coil-hole-tails)
+  hypertee-coil-hole
+  'hypertee-coil-hole (current-inspector) (auto-write) (auto-equal))
+; TODO: Define `hypertee-furl` in terms of `hypertee-furl-unchecked`.
+(define-imitation-simple-struct
+  (hypertee? hypertee-dim-sys hypertee-unfurl)
+  hypertee-furl-unchecked
+  'hypertee (current-inspector) (auto-write) (auto-equal))
+
+(define (hypertee/c ds)
+  (rename-contract
+    (expectfn (hypertee-furl-unchecked actual-ds _) #f
+      (dim-sys-accepts? ds actual-ds))
+    `(hypertee/c ,ds)))
+
+(define (hypertee-map-cps ds state ht on-hole then)
+  (w- ss (hypertee-snippet-sys ds)
+  #/dissect ht (hypertee-furl-unchecked _ coil)
+  #/mat coil (hypertee-coil-zero)
+    (then state #/hypertee-furl-unchecked ds #/hypertee-coil-zero)
+  #/dissect coil (hypertee-coil-hole d hole data tails)
+  #/on-hole state hole data #/fn state data
+  #/hypertee-map-cps ds state tails
+    (fn state hole tail then
+      (w- d (snippet-sys-snippet-degree ss hole)
+      #/hypertee-map-cps ds state tail
+        (fn state hole data then
+          (if
+            (dim-sys-dim<? ds
+              (snippet-sys-snippet-degree ss hole)
+              d)
+            (dissect data (trivial)
+              (then state #/trivial))
+            (on-hole state hole data then)))
+        then))
+  #/fn state tails
+  #/then state #/hypertee-furl-unchecked ds
+    (hypertee-coil-hole d hole data tails)))
+
+(define (hypertee-each-cps ds state ht on-hole then)
+  
+  ; TODO: Pick just one of these implementations.
+  
+  #;
+  (hypertee-map-cps ds state ht
+    (fn state hole data then
+      (on-hole state hole data #/fn state
+      #/then state data))
+  #/fn state mapped
+  #/then state)
+  
+  (w- ss (hypertee-snippet-sys ds)
+  #/dissect ht (hypertee-furl-unchecked _ coil)
+  #/mat coil (hypertee-coil-zero) (then state)
+  #/dissect coil (hypertee-coil-hole d hole data tails)
+  #/on-hole state hole data #/fn state
+  #/hypertee-each-cps ds state tails
+    (fn state hole tail then
+      (w- d (snippet-sys-snippet-degree ss hole)
+      #/hypertee-each-cps ds state tail
+        (fn state hole data then
+          (if
+            (dim-sys-dim<? ds
+              (snippet-sys-snippet-degree ss hole)
+              d)
+            (dissect data (trivial)
+              (then state))
+            (on-hole state hole data then)))
+        then))
+      then))
+
+(define (hypertee-each-ltr ds state ht on-hole)
+  (hypertee-each-cps ds state ht
+    (fn state hole data then #/then #/on-hole state hole data)
+  #/fn state
+    state))
+
+(define (hypertee-zip-map ds shape snippet hvv-to-maybe-v)
+  (w- ss (hypertee-snippet-sys ds)
+  #/dissect shape (hypertee-furl-unchecked _ shape-coil)
+  #/dissect snippet (hypertee-furl-unchecked _ snippet-coil)
+  #/mat shape-coil (hypertee-coil-zero)
+    (expect snippet-coil (hypertee-coil-zero) (nothing)
+    #/just #/hypertee-furl-unchecked ds #/hypertee-coil-zero)
+  #/dissect shape-coil
+    (hypertee-coil-hole shape-d shape-hole shape-data shape-tails)
+  #/expect snippet-coil
+    (hypertee-coil-hole
+      snippet-d snippet-hole snippet-data snippet-tails)
+    (nothing)
+  #/expect (dim-sys-dim=? ds shape-d snippet-d) #t (nothing)
+  #/expect
+    (snippet-sys-shape=? ss shape-hole snippet-hole
+    #/fn hole shape-data snippet-data
+      (dissect shape-data (trivial)
+      #/dissect snippet-data (trivial)
+        #t))
+    #t
+    (nothing)
+  #/maybe-bind (hvv-to-maybe-v shape-hole shape-data snippet-data)
+  #/fn result-data
+  #/maybe-bind
+    (snippet-sys-snippet-zip-map ss shape-tails snippet-tails
+    #/fn hole shape-tail snippet-tail
+      (w- d (snippet-sys-snippet-degree ss hole)
+      #/snippet-sys-snippet-zip-map-selective ss
+        shape-tail
+        (snippet-sys-snippet-map ss snippet-tail #/fn hole data
+          (if
+            (dim-sys-dim<? ds (snippet-sys-snippet-degree ss hole) d)
+            (dissect data (trivial)
+              (selected #/trivial))
+            data))
+      #/fn hole shape-data snippet-data
+        (if (dim-sys-dim<? ds (snippet-sys-snippet-degree ss hole) d)
+          (dissect shape-data (trivial)
+          #/dissect snippet-data (trivial)
+            (just #/trivial))
+          (hvv-to-maybe-v hole shape-data snippet-data))))
+  #/fn result-tails
+  #/just #/hypertee-furl-unchecked ds
+    (hypertee-coil-hole shape-d shape-hole result-data result-tails)))
+
+(define-imitation-simple-struct
+  (hypertee-snippet-sys? hypertee-snippet-sys-dim-sys)
+  hypertee-snippet-sys
+  'hypertee-snippet-sys (current-inspector) (auto-write) (auto-equal)
+  (#:prop prop:snippet-sys #/make-snippet-sys-impl-from-various-1
+    ; snippet-sys-dim-sys
+    (dissectfn (hypertee-snippet-sys ds)
+      ds)
+    ; snippet-sys-shape-snippet-sys
+    (fn ss
+      ss)
+    ; snippet-sys-snippet/c
+    (dissectfn (hypertee-snippet-sys ds)
+      (hypertee/c ds))
+    ; snippet-sys-snippet-degree
+    (fn ss snippet
+      (dissect ss (hypertee-snippet-sys ds)
+      #/dissect snippet (hypertee-furl-unchecked _ coil)
+      #/mat coil (hypertee-coil-zero) (dim-sys-dim-zero ds)
+      #/dissect coil (hypertee-coil-hole d hole data tails) d))
+    ; snippet-sys-shape->snippet
+    (fn ss shape
+      shape)
+    ; snippet-sys-snippet->maybe-shape
+    (fn ss snippet
+      (just snippet))
+    ; snippet-sys-snippet-set-degree-maybe
+    (fn ss degree snippet
+      (dissect ss (hypertee-snippet-sys ds)
+      #/dissect snippet (hypertee-furl-unchecked _ coil)
+      #/mat coil (hypertee-coil-zero)
+        (expect (dim-sys-dim=0? ds degree) #t (nothing)
+        #/just #/hypertee-furl-unchecked ds #/hypertee-coil-zero)
+      #/dissect coil (hypertee-coil-hole d hole data tails)
+        (expect
+          (and
+            (not #/dim-sys-dim=0? ds degree)
+            (dim-sys-dim<? ds
+              (snippet-sys-snippet-degree ss hole)
+              degree))
+          #t
+          (nothing)
+        #/maybe-map
+          (snippet-sys-snippet-map-maybe ss tails #/fn hole tail
+            (snippet-sys-snippet-set-degree-maybe ss degree tail))
+        #/fn tails
+          (hypertee-furl-unchecked ds
+            (hypertee-coil-hole d hole data tails)))))
+    ; snippet-sys-snippet-done
+    (fn ss degree shape data
+      (dissect ss (hypertee-snippet-sys ds)
+      #/hypertee-furl-unchecked ds #/hypertee-coil-hole
+        degree
+        (snippet-sys-snippet-map ss shape #/fn hole data #/trivial)
+        data
+        (snippet-sys-snippet-map ss shape #/fn hole data
+          (snippet-sys-snippet-done ss degree hole data))))
+    ; snippet-sys-snippet-undone
+    (fn ss snippet
+      (dissect ss (hypertee-snippet-sys ds)
+      #/dissect snippet (hypertee-furl-unchecked _ coil)
+      #/expect coil (hypertee-coil-hole d hole data tails) (nothing)
+      #/maybe-map
+        (snippet-sys-snippet-map-maybe ss tails #/fn hole tail
+          (maybe-bind (snippet-sys-snippet-undone ss tail)
+          #/dissectfn (list tail-d tail-shape data)
+          #/expect
+            (and
+              (dim-sys-dim=? ds d tail-d)
+              (snippet-sys-shape=? ss hole tail-shape
+              #/fn hole hole-data tail-shape-data
+                (dissect hole-data (trivial)
+                #/dissect tail-shape-data (trivial)
+                  #t)))
+            #t
+            (nothing)
+          #/just data))
+      #/fn shape
+        (list d shape data)))
+    ; snippet-sys-snippet-splice
+    (fn ss snippet hv-to-splice
+      (dissect ss (hypertee-snippet-sys ds)
+      #/w-loop next
+        snippet snippet
+        first-nontrivial-d (dim-sys-dim-zero ds)
+        
+        (dissect snippet (hypertee-furl-unchecked _ coil)
+        #/mat coil (hypertee-coil-zero)
+          (just #/hypertee-furl-unchecked ds #/hypertee-coil-zero)
+        #/dissect coil (hypertee-coil-hole d hole data tails)
+        #/maybe-bind
+          (if
+            (dim-sys-dim<? ds
+              (snippet-sys-snippet-degree ss hole)
+              first-nontrivial-d)
+            (dissect data (trivial)
+              (unselected #/trivial))
+            (hv-to-splice hole data))
+        #/fn splice
+        #/mat splice (unselected data)
+          (maybe-bind
+            (snippet-sys-snippet-map ss tails #/fn hole tail
+              (next tail
+                (dim-sys-dim-max ds
+                  first-nontrivial-d
+                  (snippet-sys-snippet-degree ss hole))))
+          #/fn tails
+          #/just #/hypertee-furl-unchecked ds
+            (hypertee-coil-hole d hole data tails))
+        #/dissect splice (selected suffix)
+        #/w- suffix
+          (snippet-sys-snippet-map ss suffix #/fn hole data
+            (expect data (unselected data) data
+            #/unselected #/unselected data))
+        #/maybe-bind
+          (snippet-sys-snippet-zip-map-selective ss tails suffix
+          #/fn hole tail data
+            (w- d (snippet-sys-snippet-degree ss hole)
+            #/dissect data (trivial)
+            #/selected
+              (snippet-sys-snippet-select-if-degree< ss d tail)))
+        #/fn suffix
+        #/snippet-sys-snippet-splice ss suffix #/fn hole data data)))
+    ; snippet-sys-snippet-zip-map-selective
+    (fn ss shape snippet hvv-to-maybe-v
+      
+      ; TODO: See if we can implement this in a more direct style like
+      ; `hypertee-zip-map` rather than using `hypertee-map-cps`.
+      ;
+      ; When the first hole was `selected`, the implementation would
+      ; be just like `hypertee-zip-map`.
+      ;
+      ; When the first hole was `unselected`, we would have to do
+      ; something like this: Join the `tails` as a `selective-snippet`
+      ; with some unselected holes in between, zip it again that way,
+      ; and then parse those unselected holes to turn the result back
+      ; into a replacement for `tails`.
+      
+      (dissect ss (hypertee-snippet-sys ds)
+      #/w- numbered-snippet
+        (hypertee-map-cps ds 0 snippet
+          (fn state hole data then
+            (mat data (unselected data)
+              (then state #/unselected data)
+            #/dissect data (selected data)
+              (then (add1 state) #/selected #/list state data)))
+        #/fn state numbered-snippet
+          numbered-snippet)
+      #/maybe-bind
+        (snippet-sys-snippet-filter-maybe ss numbered-snippet)
+      #/fn filtered-numbered-snippet
+      #/maybe-bind
+        (hypertee-zip-map ds shape filtered-numbered-snippet
+        #/fn hole shape-data snippet-data
+          (dissect snippet-data (list i snippet-data)
+          #/maybe-map (hvv-to-maybe-v shape-data snippet-data)
+          #/fn result-data
+            (list i result-data)))
+      #/fn filtered-numbered-result
+      #/w- env
+        (hypertee-each-ltr ds (make-immutable-hasheq)
+          filtered-numbered-result
+        #/fn env hole data
+          (dissect data (list i data)
+          #/hash-set env i data))
+      #/snippet-sys-snippet-map-selective ss numbered-snippet
+      #/fn hole data
+        (dissect data (list i data)
+        #/hash-ref env i)))))
