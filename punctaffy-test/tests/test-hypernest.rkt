9edcd9f3580c454a913da41aeb2e54112de908e7
Ross Angle <rokitna@hotmail.com>
2020-02-25 16:25:54 -0800

    INCOMPLETE: In test-hypernest.rkt, rename a local variable from `tails-hypertee` to `tails-hypernest`, which it should have been all along.

--- punctaffy-test/tests/test-hypernest.rkt
+++ punctaffy-test/tests/test-hypernest.rkt
@@ -5,7 +5,7 @@
 ; Unit tests of the hypernest data structure for hypersnippet-shaped
 ; data.
 
-;   Copyright 2018-2019 The Lathe Authors
+;   Copyright 2018-2020 The Lathe Authors
 ;
 ;   Licensed under the Apache License, Version 2.0 (the "License");
 ;   you may not use this file except in compliance with the License.
@@ -42,10 +42,10 @@
   (hypernest-furl ds
   #/hypernest-coil-hole overall-degree data tails-hypertee))
 
-(define (hno overall-degree data bump-degree tails-hypertee)
+(define (hno overall-degree data bump-degree tails-hypernest)
   (hypernest-furl ds
   #/hypernest-coil-bump
-    overall-degree data bump-degree tails-hypertee))
+    overall-degree data bump-degree tails-hypernest))
 
 
 
